444dd98c833684d833c590c30ae8b300a948b74c
josecostv <zeca.gremio2000@gmail.com>
2022-11-15 16:04:49 -0300

    nova pagina de links

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+const themeSwitch = document.getElementById("switch");
+const html = document.documentElement;
+console.log(html);
+html.classList.add("dark");
+
+themeSwitch.addEventListener("change", ({ target: { checked } }) => {
+  if (!checked) {
+    html.classList.add("dark");
+  } else {
+    html.classList.remove("dark");
+  }
+});
